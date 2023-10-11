52abc85f8d688a193509cb01461e9be56eb7b0e8
Mauricio Gracia <mauriciogracia@gmail.com>
2023-10-10 22:22:51 -0500

    database bootstrapping

--- build/database/00-create-db.sql
+++ build/database/00-create-db.sql
@@ -1,12 +1,25 @@
-CREATE DATABASE DemoDb;
+CREATE DATABASE PermissionsDB;
 GO
-USE DemoDb;
+USE PermissionsDB;
 GO
 
-CREATE TABLE [dbo].[Table1]
-(
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
-    [Name] NVARCHAR(200) NOT NULL,
-)
+CREATE TABLE PERMISOS (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    NombreEmpleado NVARCHAR(100) NOT NULL,
+    ApellidoEmpleado NVARCHAR(100) NOT NULL,
+    TipoPermiso INT NOT NULL,
+    FechaPermiso DATE NOT NULL
+);
+
+CREATE TABLE TIPO_PERMISOS (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Descripcion NVARCHAR(100) NOT NULL
+);
+
+-- Add a foreign key constraint to establish the relationship
+ALTER TABLE PERMISOS
+ADD CONSTRAINT FK_Permission_TipoPermiso
+FOREIGN KEY (TipoPermiso) 
+REFERENCES TIPO_PERMISOS(Id);
 
 GO
--- build/sqlserver.Dockerfile
+++ build/sqlserver.Dockerfile
@@ -10,10 +10,6 @@ RUN mkdir -p /var/opt/mssql/scripts
 COPY ./build/database/*.sql /var/opt/mssql/scripts/
 WORKDIR /var/opt/mssql/scripts
 
-# Start SQL Server and execute the SQL script
-#CMD /bin/bash -c "/opt/mssql/bin/sqlservr & /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i 00-create-db.sql"
-#CMD /bin/bash -c "/opt/mssql/bin/sqlservr & sleep 30 && /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -i 00-create-db.sql"
-
 # Start SQL Server and execute the SQL script after it becomes available
 CMD /bin/bash -c "/opt/mssql/bin/sqlservr & \
                   while ! /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q 'SELECT 1' ; do \
