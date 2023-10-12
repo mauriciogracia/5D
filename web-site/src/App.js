ac1366eccbb0f453022c82f75c1d750bdd8c34e4
Mauricio Gracia <mauriciogracia@gmail.com>
2023-10-11 23:01:42 -0500

    basic add-persmissions forms

--- web-site/src/App.js
+++ web-site/src/App.js
@@ -1,15 +1,24 @@
-// App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-import AddPermission from "./permissions/add.jsx"; // Import your component
+// Import your components
+import AddPermission from "./permissions/add-permision.jsx";
+
+function Home() {
+    return (
+        <div>
+            <h1>HOME PAGE</h1>
+            <Link to="/add-permission">Go to Add Permission</Link>
+        </div>
+    );
+}
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/add-permission" component={AddPermission} />
-                {/* Add other routes as needed */}
+                <Route path="/" element={<Home />} />
+                <Route path="/add-permission" element={<AddPermission />} />
             </Routes>
         </Router>
     );
